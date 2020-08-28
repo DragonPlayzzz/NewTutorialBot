2ad34c27a092dc7ca86d7412a1c2db4512f07c39
DragonPlayzzz <68904506+DragonPlayzzz@users.noreply.github.com>
2020-08-28 20:46:29 +0100

    Create invite.js

---
+++ commands/info/invite.js
@@ -0,0 +1,4 @@
+if (message.content === 'd!invite') {
+	// send back "Pong." to the channel the message was sent in
+	message.channel.send('https://discord.com/oauth2/authorize?client_id=747749013851340821&scope=bot&permissions=8');
+}
